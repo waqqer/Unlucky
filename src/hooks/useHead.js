cd83e539967a6a2f2caf4eff3ebd7823482c286c
waq <woqqa047@gmail.com>
2026-03-24 21:37:49 +0500

    useHead string overload

--- src/hooks/useHead.js
+++ src/hooks/useHead.js
@@ -1,6 +1,9 @@
 const minecraft_heads_url = "https://mc-heads.net/avatar/"
 
-const useHead = (user) => {
+const useHead = (data) => {
+    if(typeof data === "string") {
+        return minecraft_heads_url + data
+    }
     return minecraft_heads_url + user?.minecraftUUID ?? ""
 }
 
