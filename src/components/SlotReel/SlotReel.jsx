df58bec856ebb6cce063266a001bac9fafaaee45
waq <woqqa047@gmail.com>
2026-03-31 00:07:43 +0500

    slots new items

--- src/components/SlotReel/SlotReel.jsx
+++ src/components/SlotReel/SlotReel.jsx
@@ -2,13 +2,21 @@ import diamondImg from "@/shared/images/games/slots/diamond.png"
 import goldImg from "@/shared/images/games/slots/gold.png"
 import ironImg from "@/shared/images/games/slots/iron.png"
 import coalImg from "@/shared/images/games/slots/coal.png"
+import amethystImg from "@/shared/images/games/slots/amethyst.png"
+import monyaImg from "@/shared/images/games/slots/monya.png"
+import starImg from "@/shared/images/games/slots/star.png"
+import redstoneImg from "@/shared/images/games/slots/redstone.png"
 import styles from "./SlotReel.module.css"
 
 const SYMBOL_IMAGES = {
     diamond: diamondImg,
     gold: goldImg,
     iron: ironImg,
-    coal: coalImg
+    coal: coalImg,
+    star: starImg,
+    monya: monyaImg,
+    redstone: redstoneImg,
+    amethyst: amethystImg
 }
 
 const SlotReel = (props) => {
