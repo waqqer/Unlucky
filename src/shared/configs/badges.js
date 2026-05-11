eb30f3c9f0b1690e8f97ad8cd51898bcfdcbcfff
waq <woqqa047@gmail.com>
2026-05-11 22:54:51 +0500

    new badges

--- src/shared/configs/badges.js
+++ src/shared/configs/badges.js
@@ -4,6 +4,8 @@ import MINER_BADGE from "@/shared/images/badges/miner_badge.webp"
 import ROCKET_BADGE from "@/shared/images/badges/rocket_badge.webp"
 import SLOTS_BADGE from "@/shared/images/badges/slots_badge.webp"
 import WINNER_BADGE from "@/shared/images/badges/winner_badge.webp"
+import RICH_BADGE from "@/shared/images/badges/rich_badge.webp"
+import LUCKY_BADGE from "@/shared/images/badges/lucky_badge.webp"
 
 export const BADGES_CONFIG = {
     badges: {
@@ -33,6 +35,20 @@ export const BADGES_CONFIG = {
             descripton: "Даётся за первую победу в любой из игр",
             icon: WINNER_BADGE,
             quality: "GOOD"
+        },
+
+        "lucky": {
+            title: "Везунчик",
+            descripton: "Капуцк, ты везунчик! Целых 5 побед подряд...",
+            icon: WINNER_BADGE,
+            quality: "EPIC"
+        },
+
+        "rich": {
+            title: "Богач",
+            descripton: "Даётся за выйгрышь больше 1000 ар.",
+            icon: WINNER_BADGE,
+            quality: "EPIC"
         },
     },
 
