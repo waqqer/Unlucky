78836005d6a2e99ac8be480e3af494f098655d5b
waq <woqqa047@gmail.com>
2026-05-29 22:31:43 +0500

    new placeholder

--- src/components/Placeholder/Placeholder.jsx
+++ src/components/Placeholder/Placeholder.jsx
@@ -1,13 +1,44 @@
 import { memo } from "react"
 import styles from "./Placeholder.module.css"
 
+const delay = {
+    min: 0.1,
+    max: 1
+}
+
+const duration = {
+    min: 3,
+    max: 10
+}
+
+const getLoaderStyle = (data) => {
+    const del = Math.random() * (delay.max - delay.min) + delay.min;
+    const dur = Math.random() * (duration.max - duration.min) + duration.min;
+    return {
+        animationDelay: `${del}s`,
+        animationDuration: `${dur}s`,
+        
+        ...data
+    }
+}
+
 const Placeholder = (props) => {
     const {
         className = ""
     } = props
 
     return (
-        <span className={`${styles.loader} ${className}`}></span>
+        <div className={styles["loader-box"]}>
+            <span className={`${styles.loader} ${className}`} style={getLoaderStyle({borderTopLeftRadius: "7px"})}></span>
+            <span className={`${styles.loader} ${className}`} style={getLoaderStyle()}></span>
+            <span className={`${styles.loader} ${className}`} style={getLoaderStyle()}></span>
+            <span className={`${styles.loader} ${className}`} style={getLoaderStyle({borderTopRightRadius: "7px"})}></span>
+
+            <span className={`${styles.loader} ${className}`} style={getLoaderStyle({borderBottomLeftRadius: "7px"})}></span>
+            <span className={`${styles.loader} ${className}`} style={getLoaderStyle()}></span>
+            <span className={`${styles.loader} ${className}`} style={getLoaderStyle()}></span>
+            <span className={`${styles.loader} ${className}`} style={getLoaderStyle({borderBottomRightRadius: "7px"})}></span>
+        </div>
     )
 }
 
