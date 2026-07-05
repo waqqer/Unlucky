9c54c7a8e82c7a92968842af0e2a686be245c231
waq <woqqa047@gmail.com>
2026-07-05 23:40:54 +0500

    section picker improve

--- src/Components/Controlls/SectionPicker/SectionPicker.tsx
+++ src/Components/Controlls/SectionPicker/SectionPicker.tsx
@@ -5,7 +5,8 @@ import styles from "./SectionPicker.module.css"
 type SectionData = {
     icon?: ReactNode
     name: string
-    id: string
+    id?: string,
+    onClick?: () => {}
 }
 
 interface UISectionPicker extends Classable {
@@ -18,19 +19,23 @@ const SectionPicker = (props: UISectionPicker) => {
         sections
     } = props
 
-    const handleClick = useCallback((id: string) => {
-        document.getElementById(id).scrollIntoView({
-            behavior: "smooth"
-        })
+    const handleClick = useCallback((data: SectionData) => {
+        if (data.id) {
+            document.getElementById(data.id).scrollIntoView({
+                behavior: "smooth"
+            })
+        }
+        
+        data.onClick?.()
     }, [])
 
     return (
         <div className={`${styles.picker} ${className}`}>
             {sections.map((v, i) => (
-                <a key={i} className={styles.btn} onClick={() => handleClick(v.id)}>
+                <a key={i} className={styles.btn} onClick={() => handleClick(v)}>
                     {v.icon ?? (
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
-                            <path d="M201.4 297.4C188.9 309.9 188.9 330.2 201.4 342.7L361.4 502.7C373.9 515.2 394.2 515.2 406.7 502.7C419.2 490.2 419.2 469.9 406.7 457.4L269.3 320L406.6 182.6C419.1 170.1 419.1 149.8 406.6 137.3C394.1 124.8 373.8 124.8 361.3 137.3L201.3 297.3z"/>
+                            <path d="M201.4 297.4C188.9 309.9 188.9 330.2 201.4 342.7L361.4 502.7C373.9 515.2 394.2 515.2 406.7 502.7C419.2 490.2 419.2 469.9 406.7 457.4L269.3 320L406.6 182.6C419.1 170.1 419.1 149.8 406.6 137.3C394.1 124.8 373.8 124.8 361.3 137.3L201.3 297.3z" />
                         </svg>
                     )}
                     <span className={styles.name}>{v.name}</span>
